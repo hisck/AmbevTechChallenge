12a1e8a47ede4540f418ec1a5aa70dc963ee51e3
hisck <ra99829@uem.br>
2025-01-26 17:07:06 -0300

    chore: add saleMappingProfile

---
+++ src/Ambev.DeveloperEvaluation.WebApi/Mappings/SaleMappingProfile.cs
@@ -0,0 +1,24 @@
+﻿using Ambev.DeveloperEvaluation.Application.Sales.Common;
+using Ambev.DeveloperEvaluation.WebApi.Common;
+using Ambev.DeveloperEvaluation.WebApi.Features.Sale.Common;
+using AutoMapper;
+
+namespace Ambev.DeveloperEvaluation.WebApi.Mappings
+{
+    /// <summary>
+    /// Global mapping profile for Sale-related DTOs and responses
+    /// </summary>
+    public class SaleMappingProfile : Profile
+    {
+        /// <summary>
+        /// Initializes the common mappings for Sale entities and responses
+        /// </summary>
+        public SaleMappingProfile()
+        {
+            // Common mappings for sale responses
+            CreateMap<SaleDto, SaleResponse>();
+            CreateMap<SaleItemDto, SaleItemResponse>();
+            CreateMap<PaginatedList<SaleDto>, PaginatedResponse<SaleResponse>>();
+        }
+    }
+}
